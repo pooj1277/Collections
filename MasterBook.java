c5c9a1e3b07d2d9b8ca188eb12f7e0220a37f21f
pooj1277 <pooja.sd3@gmail.com>
2021-11-07 07:15:36 -0800

    [Added]: Ensured no duplicates get added while adding contacts to the address book

--- MasterBook.java
+++ MasterBook.java
@@ -32,15 +32,6 @@ public class MasterBook {
 				addContactsInAddressBook();
 				break;
 			case 4:
-				searchContactsByCityName();
-				break;
-			case 5:
-				searchContactByStateName();
-				break;
-			case 6:
-				viewCountOfContactsFromSpecificCityName();
-				break;
-			case 7:
 				option = true;
 			}
 		}
@@ -52,28 +43,13 @@ public class MasterBook {
 		System.out.println("\t 1. Print Menu Again.");
 		System.out.println("\t 2. Add New Address Book to the Master Book");
 		System.out.println("\t 3. Add contact in the Existing Address Book");
-		System.out.println("\t 4. Search Cntact Using City Name");
-		System.out.println("\t 5. Search Contact Using State Name : ");
-		System.out.println("\t 6. Display Contacts from Specific City : ");
-		System.out.println("\t 7. Exit");
+		System.out.println("\t 4. Exit");
 	}
 
 	public void addAddressBook() {
 		System.out.println("Enter the Name of the City for which you want to add Address Book: ");
 		String cityName = scanner.nextLine();
 		masterBook.put(cityName, new AddressBook());
-		System.out.println("Address Book Added.");
-		System.out.println("-------------------------");
-		System.out.println("Would You like to add contact in this Address Book ?");
-		System.out.println("1 to Add contact in this Address Book : ");
-		System.out.println("2 to for the main menu : ");
-		int choice = scanner.nextInt();
-		scanner.nextLine();
-		if (choice == 1) {
-			masterBook.get(cityName).mainMenuAddressBook();
-		} else {
-			mainMenuMasterBook();
-		}
 	}
 
 	public void addContactsInAddressBook() {
@@ -84,46 +60,5 @@ public class MasterBook {
 		} else {
 			masterBook.get(addressBookName).mainMenuAddressBook();
 		}
-	}
-
-	public void searchContactsByCityName() {
-		System.out.println("Enter the name of the city : ");
-		String cityName = scanner.nextLine();
-		for (Map.Entry<String, AddressBook> entry : masterBook.entrySet()) {
-			entry.getValue().searchContactByCity(cityName);
-		}
-	}
-
-	public void searchContactByStateName() {
-		System.out.println("Enter the name of State : ");
-		String stateName = scanner.nextLine();
-		for (Map.Entry<String, AddressBook> entry : masterBook.entrySet()) {
-			entry.getValue().searchContactByState(stateName);
-		}
-	}
-
-	public void viewCountOfContactsFromSpecificCityName() {
-		long totalCount = 0;
-		long count = 0;
-		System.out.println("Enter the name of the City : ");
-		String cityName = scanner.nextLine();
-		for (Map.Entry<String, AddressBook> entry : masterBook.entrySet()) {
-			count = entry.getValue().viewCountOfContactsFromSpecificCity(cityName);
-			totalCount += count;
-		}
-		System.out.println("Total contacts from " + cityName + " city is " + totalCount);
-	}
-	
-	public void viewCountOfContactFromSpecificStateName() {
-		long totalCount = 0;
-		long count = 0;
-		System.out.println("Enter the name of the State : ");
-		String stateName = scanner.nextLine();
-		for (Map.Entry<String, AddressBook> entry : masterBook.entrySet()) {
-			count = entry.getValue().viewCountOfContactsFromSpecificCity(stateName);
-			totalCount += count;
-		}
-		System.out.println("Total contacts from " + stateName + " state is " + totalCount);
-	
 	}
 }
